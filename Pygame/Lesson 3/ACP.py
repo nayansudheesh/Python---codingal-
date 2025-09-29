39ceb739ea36520a9ea4da7e0a3f1a69700b3611
nayansudheesh <nayanthejassudheesh@gmail>
2025-09-29 22:11:03 +0530

    ACP

--- Pygame/Lesson 3/ACP.py
+++ Pygame/Lesson 3/ACP.py
@@ -4,7 +4,7 @@ pygame.init()
 
 screen_width, screen_height = 500, 500
 screen = pygame.display.set_mode((screen_width, screen_height))
-pygame.display.set_caption('color changing sprite')
+pygame.display.set_caption('Sprites moving')
 RED = (139,0,0)
 x = 0
 y = 0
